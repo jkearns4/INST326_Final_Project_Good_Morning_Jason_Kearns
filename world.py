a6661644402ff5e54bd687d5b22298d5b2cf8f4f
asangi559 <asangi@terpmail.umd.edu>
2026-04-24 15:02:33 -0400

    Update world.py
    Add valid move check

--- world.py
+++ world.py
@@ -4,6 +4,23 @@ class Grid():
     
     def move_player(self, player: object, direction):
         player.move(direction)
+
+    def is_valid_move(self, location, direction):
+        row = location[0]
+        col = location[1]
+
+        if direction == "up":
+            col += 1
+        elif direction == "down":
+            col -= 1
+        elif direction == "left":
+            row -= 1
+        elif direction == "right":
+            row += 1
+        else:
+            return False
+
+        return 0 <= row < len(self._map) and 0 <= col < len(self._map) 
         
     def __str__(self):
         output_string = ""
@@ -11,7 +28,6 @@ class Grid():
             output_string += str(item) + "\n"
         return output_string
         
-
-
+   
 test_grid=Grid(5)
 print(test_grid)
