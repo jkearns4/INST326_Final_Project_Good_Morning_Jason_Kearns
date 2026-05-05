b8042290948f4fdcc8732fe4b65b12f15b64cf96
Andy Huang <ahuang22@terpmail.umd.edu>
2026-05-04 21:36:42 -0400

    Fixed indention error

--- entities.py
+++ entities.py
@@ -55,9 +55,11 @@ class Player():
         self.health -= damage
         print(f"You ({self.name}) just took {damage} damage! You now have {self.health} health left!")
         
-         if self.health <= 0:
+        if self.health <= 0:
             print("You lost all your health. Game over!")
             sys.exit()
+            
+            
     def move(self, direction: str):
         direction =  direction.lower()
         if not direction in ["up", "down", "left", "right"]:
